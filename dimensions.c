8cf9f06b1f77ad6212484b54ac0b393a8cb7ed55
Maya Cranor <mcranor@olin.edu>
2023-02-24 15:19:35 -0500

    Wrote code to find dimensions of terminal.

--- dimensions.c
+++ dimensions.c
@@ -1,1 +1,16 @@
-//test file right here!
+//https://medium.com/@pauljlucas/getting-the-terminal-width-in-c-48560b556575
+//https://psychocod3r.wordpress.com/2019/02/25/how-to-get-the-dimensions-of-a-linux-terminal-window-in-c/
+//https://stackoverflow.com/questions/1022957/getting-terminal-width-in-c
+#include <sys/ioctl.h>
+#include <stdio.h>
+
+
+int main (void)
+{
+    struct winsize w;
+    ioctl(0, TIOCGWINSZ, &w);
+
+    printf ("lines %d\n", w.ws_row);
+    printf ("columns %d\n", w.ws_col);
+    return 0;
+}
